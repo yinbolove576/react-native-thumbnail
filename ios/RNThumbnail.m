6aac0b25f40d1bd1773ab9e50ae3b87e024c8d76
Yin <yinbolove576@sina.com>
2021-10-11 12:44:54 +0800

    1、调整iOS缩略图质量

--- ios/RNThumbnail.m
+++ ios/RNThumbnail.m
@@ -19,22 +19,22 @@ RCT_EXPORT_METHOD(get:(NSString *)filepath resolve:(RCTPromiseResolveBlock)resol
         filepath = [filepath stringByReplacingOccurrencesOfString:@"file://"
                                                   withString:@""];
         NSURL *vidURL = [NSURL fileURLWithPath:filepath];
-        
+
         AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:vidURL options:nil];
         AVAssetImageGenerator *generator = [[AVAssetImageGenerator alloc] initWithAsset:asset];
         generator.appliesPreferredTrackTransform = YES;
-        
+
         NSError *err = NULL;
         CMTime time = CMTimeMake(1, 60);
-        
+
         CGImageRef imgRef = [generator copyCGImageAtTime:time actualTime:NULL error:&err];
         UIImage *thumbnail = [UIImage imageWithCGImage:imgRef];
         // save to temp directory
         NSString* tempDirectory = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory,
                                                                        NSUserDomainMask,
                                                                        YES) lastObject];
-        
-        NSData *data = UIImageJPEGRepresentation(thumbnail, 1.0);
+
+        NSData *data = UIImageJPEGRepresentation(thumbnail, 0.5);
         NSFileManager *fileManager = [NSFileManager defaultManager];
         NSString *fullPath = [tempDirectory stringByAppendingPathComponent: [NSString stringWithFormat:@"thumb-%@.jpg", [[NSProcessInfo processInfo] globallyUniqueString]]];
         [fileManager createFileAtPath:fullPath contents:data attributes:nil];
@@ -49,4 +49,4 @@ RCT_EXPORT_METHOD(get:(NSString *)filepath resolve:(RCTPromiseResolveBlock)resol
 }
 
 @end
-  
+
